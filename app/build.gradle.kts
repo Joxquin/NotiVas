8ab3dc0f67306fb8acb98bd85ba73f6102155536
Joxquin <jotajoxquin@gmail.com>
2026-09-12 18:15:52 -0500

    chore(release): bump version to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.notivas"
         minSdk = 33
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.0.0"
+        versionCode = 4
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
